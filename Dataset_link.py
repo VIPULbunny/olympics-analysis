639b0ffa722bd88c47b293b2f9b1eb66264d42ad
Vipul Solanki <157818450+VIPULbunny@users.noreply.github.com>
2025-03-04 21:31:31 +0530

    Add files via upload

---
+++ Dataset_link.py
@@ -0,0 +1,11 @@
+'''
+This file contains the links to the datasets used in the project.
+downlaod the datasets from the links and save them in the same directory as the project files.
+datasets used:
+1. noc_regions.csv
+2. athlete_events.csv
+NOTE: Download both and check the names of the files before running the project.
+
+https://www.kaggle.com/datasets/heesoo37/120-years-of-olympic-history-athletes-and-results
+
+'''
